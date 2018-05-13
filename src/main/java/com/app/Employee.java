b14b9597915dd9d9640eed52e873a8e060b434ca
DELL-PC <DELL-PC@192.168.0.5>
2018-05-13 09:45:15 +0530

    changed employee java

--- src/main/java/com/app/Employee.java
+++ src/main/java/com/app/Employee.java
@@ -3,6 +3,39 @@ public class Employee {
 	private int id;
 	private String ename;
 	private double sal;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public double getSal() {
+		return sal;
+	}
+	public void setSal(double sal) {
+		this.sal = sal;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", ename=" + ename + ", sal=" + sal + "]";
+	}
+	public Employee(int id, String ename, double sal) {
+		super();
+		this.id = id;
+		this.ename = ename;
+		this.sal = sal;
+	}
+	public Employee() {
+	}
+
+
+
 }
 
 
